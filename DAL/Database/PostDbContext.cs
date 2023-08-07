309271f0107b619c24bd62365134fa8a5cb3ba54
AmrAmer198 <68470401+AmrAmer198@users.noreply.github.com>
2023-08-07 13:18:49 +0300

    Add project files.

---
+++ DAL/Database/PostDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Database
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ DAL/Repository/PostRepositroy.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Database;
+using DAL.Entity;
+using DAL.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class PostRepositroy : IPost
+    {
+
+        private ApplicationDbContext _Context { get; set; }
+
+        public PostRepositroy(ApplicationDbContext Context)
+        {
+            this._Context = Context;
+        }
+        public IEnumerable<Post> GetAllPost()
+        {
+            return _Context.Posts.ToList();
+        }
+
+        public Post GetPostById(int Id)
+        {
+            return _Context.Posts.Find(Id);
+        }
+
+        public void InsertPost(Post post)
+        {
+            _Context.Posts.Add(post);
+        }
+
+        public void UpdatePost(Post post)
+        {
+            _Context.Entry(post).State = EntityState.Modified;
+
+        }
+    }
+}
